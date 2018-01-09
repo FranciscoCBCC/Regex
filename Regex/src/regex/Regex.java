de41e92a1b0fad07d2e040dfb06ebf732006d236
Francisco <francisco.nascimento@icen.ufpa.br>
2018-01-09 13:37:45 -0300

    Atd

--- Regex/src/regex/Regex.java
+++ Regex/src/regex/Regex.java
@@ -15,7 +15,8 @@ public class Regex {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Teste
+		int a;
     }
     
 }
